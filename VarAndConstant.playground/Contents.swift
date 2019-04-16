88c045dcee0ff68bc9073a604949a05894221c00
masterUNG <phrombutr@gmail.com>
2019-04-16 11:28:08 +0700

    Variable and Constant

---
+++ VarAndConstant.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//Explicit Variable
+var myName = "Master UNG"
+var myNumber = 1234
+var myNumber2 = 3.1416
+var status = true
+
+//Implicit Varialble
+var mySurname: String = "ABC"   //String Type
+var myNumber3: Int = Int(555.5)
+var myNumber4: Double = 12.34
+var status2: Bool = false
+
+
+
+
+
+
+
